cc8f2e1629df56834ad2d65ac0cee69e85cbdaaf
SVI73 <eliasbussanich@gmail.com>
2023-08-31 03:49:37 -0300

    Corregido TextComparisonViewModelTest.kt

--- app/src/test/java/com/curso/android/app/practica/comparadordestrings/TextComparisonViewModelTest.kt
+++ app/src/test/java/com/curso/android/app/practica/comparadordestrings/TextComparisonViewModelTest.kt
@@ -1,11 +1,17 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.curso.android.app.practica.comparadordestrings.TextComparisonViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class TextComparisonViewModelTest {
 
     private lateinit var viewModel: TextComparisonViewModel
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setup() {
